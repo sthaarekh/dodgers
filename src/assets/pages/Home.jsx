0ab5c56235a10ad958178181d0795a35f46557a9
manishbhattarai8 <manishbhattarai8@gmail.com>
2024-12-05 17:26:12 +0545

    on click goto call

--- src/assets/pages/Home.jsx
+++ src/assets/pages/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
   const images = [
@@ -12,12 +13,19 @@ const Home = () => {
     'https://via.placeholder.com/150',
   ];
 
+  const navigate = useNavigate();
+
+  const handleImageClick = () => {
+    navigate('/calls');
+  };
+
   return (
-    <div className="h-screen w-screen grid grid-cols-4 grid-rows-2">
+    <div className="h-screen w-screen grid grid-cols-4 grid-rows-2 ">
       {images.map((image, index) => (
         <div
-          className="flex justify-center items-center bg-gray-200"
+          className="flex justify-center items-center bg-gray-200 hover:bg-gray-700 transition duration-300 cursor-pointer"
           key={index}
+          onClick={handleImageClick}
         >
           <div className="w-48 h-48 rounded-full overflow-hidden">
             <img
