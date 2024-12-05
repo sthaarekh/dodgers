67b8519573455c416eedfc0bfa20f0e2adf0b9cb
sthaarekh <arekhshrestha@gmail.com>
2024-12-05 10:10:17 +0545

    Tailwind setup

---
+++ src/assets/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      <div className='container'>
+        <p className='text-3xl text-blue-200'>Hello World</p>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
